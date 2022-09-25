e6ae488d58574c33d2e451affc993f39a0fc7d7f
Димка я <  @mail.ru>
2022-09-25 08:31:02 +0300

    Task13 vivesti third number

---
+++ TASK13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n > 1000)
+{
+    n = n / 10;
+Console.WriteLine($"Третья цифра числа: {n % 10}");
+}
+if (n < 100)
+Console.WriteLine("Третьей цифры нет");
