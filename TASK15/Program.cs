0e2f7c3cff054719381f7d64f804a7a14c06003b
Димка я <  @mail.ru>
2022-09-25 08:38:34 +0300

    Task 15 dni nedeli

---
+++ TASK15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите день недели(1-7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<1 || n>7)
+{
+    Console.Write("пробуй снова: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n <=5)
+    Console.WriteLine("нет");
+if(n>5)
+    Console.WriteLine("да");
